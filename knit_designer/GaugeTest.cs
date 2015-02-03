5f76d3946ad93908c8884ac337bcc9219ac1185c
thatknitter <kblewis.flt@gmail.com>
2015-02-03 13:49:15 -0600

    first gauge tests

---
+++ knit_designer/GaugeTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace knit_designer
+{
+    [TestClass]
+    public class GaugeTest
+    {
+        [TestMethod]
+        public void TestGaugeEntered()
+        {
+            Gauge gauge = new Gauge(8);
+            Assert.AreEqual(8, gauge.PerInch);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGaugeValid()
+        {
+            Gauge gauge = new Gauge(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGaugeValidNegative()
+        {
+            Gauge gauge = new Gauge(-1);
+        }
+
+
+    }
+}
