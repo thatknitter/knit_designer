1a041c7afcbf13d62fe4b68caf4b58b152957a24
thatknitter <kblewis.flt@gmail.com>
2015-02-05 15:50:28 -0600

    Finished zero state testing, problems initializing

--- KnitDesigner/Gauge.cs
+++ KnitDesigner/Gauge.cs
@@ -5,17 +5,17 @@ using System.Text;
 
 namespace knit_designer
 {
-    class Gauge
-    {
-        private decimal 
+    //class Gauge
+    //{
+    //    private decimal 
 
-        public Gauge(int PerInch)
-        {
-            if (PerInch <= 0)
-            {
-                throw new ArgumentException("Please enter a valid number.");
-            }
-            this.PerInch = PerInch;
-        }
-    }
+    //    public Gauge(int PerInch)
+    //    {
+    //        if (PerInch <= 0)
+    //        {
+    //            throw new ArgumentException("Please enter a valid number.");
+    //        }
+    //        this.PerInch = PerInch;
+    //    }
+    //}
 }
--- knit_designer/GaugeTest.cs
+++ knit_designer/GaugeTest.cs
@@ -6,33 +6,33 @@ namespace knit_designer
     [TestClass]
     public class GaugeTest
     {
-        [TestMethod]
-        public void TestGaugeEntered()
-        {
-            Gauge gauge = new Gauge(8);
-            Assert.AreEqual(8, gauge.PerInch);
-        }
+        //[TestMethod]
+        //public void TestGaugeEntered()
+        //{
+        //    Gauge gauge = new Gauge(8);
+        //    Assert.AreEqual(8, gauge.PerInch);
+        //}
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TestGaugeValid()
-        {
-            Gauge gauge = new Gauge(0);
-        }
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void TestGaugeValid()
+        //{
+        //    Gauge gauge = new Gauge(0);
+        //}
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TestGaugeValidNegative()
-        {
-            Gauge gauge = new Gauge(-1);
-        }
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        //public void TestGaugeValidNegative()
+        //{
+        //    Gauge gauge = new Gauge(-1);
+        //}
 
-        [TestMethod]
-        public void TestGagueStFourInch()
-        {
-            Gauge gauge = new Gauge(8);
-            gauge.FourIn(4);
-            Assert.AreEqual(32, gauge.FourIn);
-        }
+        //[TestMethod]
+        //public void TestGagueStFourInch()
+        //{
+        //    Gauge gauge = new Gauge(8);
+        //    gauge.FourIn(4);
+        //    Assert.AreEqual(32, gauge.FourIn);
+        //}
     }
 }
