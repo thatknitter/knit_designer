089c0d09ed79ae5521b61d9fc07e75452d4388df
thatknitter <kblewis.flt@gmail.com>
2015-02-13 14:48:14 -0600

    Waist tests written and pass

---
+++ MainWindow/Waist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnitDesigner
+{
+    public class Waist : Bust
+    {
+        public Waist(int circ) : base(circ)
+        {
+
+        }
+    }
+}
---
+++ knit_designer/TestWaist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using KnitDesigner;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKnitDesigner
+{
+    [TestClass]
+    public class TestWaist
+    {
+        [TestMethod]
+        public void TestWaistExists()
+        {
+            Waist waist = new Waist(25);
+            Assert.AreEqual(25, waist.Circ);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWaistNotZero()
+        {
+            Waist waist = new Waist(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWaistPositive()
+        {
+            Waist waist = new Waist(-1);
+        }
+
+        [TestMethod]
+        public void TestWaistScale()
+        {
+            Waist waist = new Waist(25);
+            waist.Gauge(6);
+            Assert.AreEqual(150, waist.Circ);
+        }
+    }
+}
