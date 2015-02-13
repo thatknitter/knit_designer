b28836247934f81e0f10e86380d0d00013840c0f
thatknitter <kblewis.flt@gmail.com>
2015-02-13 14:58:27 -0600

    Hem tests pass

---
+++ MainWindow/Hem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KnitDesigner
+{
+    public class Hem : Bust
+    {
+        public Hem(int circ) : base(circ) 
+        {
+
+        }
+    }
+}
