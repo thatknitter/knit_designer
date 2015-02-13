5656506eecb3d7d3e3412c9d8db7d71bacb22694
thatknitter <kblewis.flt@gmail.com>
2015-02-13 15:09:10 -0600

    shoulder width tests pass

---
+++ MainWindow/Shoulder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnitDesigner
+{
+    public class Shoulder
+    {
+        private decimal width;
+        public decimal Width
+        {
+            get { return this.width; }
+        }
+
+        public Shoulder(int width)
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentException("Please enter a valid bust measurement.");
+            }
+            this.width = width;
+        }
+
+        public decimal Gauge(int width) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
