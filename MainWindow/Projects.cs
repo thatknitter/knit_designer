a2678b5d85137b0fe206086143d6d662b22aac0d
thatknitter <kblewis.flt@gmail.com>
2015-02-24 11:41:47 -0600

    Still afraid of UI testing, started project db

---
+++ MainWindow/Projects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace KnitDesigner
+{
+    public class Projects
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+
+    }
+
+    public class MainWindowContent
+    {
+
+    }
+}
---
+++ knit_designer/OpeningTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKnitDesigner
+{
+    [TestClass]
+    public class OpeningTest
+
+        //As a user
+        //In order to get started
+        //I want to open an existing file
+
+    {
+        [TestMethod]
+        public void ()
+        {
+        }
+    }
+}
